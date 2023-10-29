be873fd85bba0fdf169af58d1c265914362f171e
IshaqNiloy <ishaqniloy1996@gmail.com>
2023-10-30 03:02:39 +0600

    problem solved

--- 135. Candy.py
+++ 135. Candy.py
@@ -4,39 +4,28 @@ from typing import List
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         list_size = len(ratings)
+        # since every child must get at least one candy
         list_of_candy = [1] * list_size
-        min_num_of_candies = len(ratings)
-        for curr in range(len(ratings)):
-            previous = curr - 1
-            next = curr + 1
-            print(list_of_candy)
-            # case -> general case
-            if previous >= 0 and next < len(ratings):
-                if ratings[curr] > ratings[previous] or ratings[curr] > ratings[next]:
-                    if ratings[curr] > ratings[previous]:
-                        min_num_of_candies += list_of_candy[previous]
-                        list_of_candy[curr] = list_of_candy[previous] + 1
-                    else:
-                        min_num_of_candies += 1
-                        list_of_candy[curr] += 1
-            # case -> 2 when the current index is at the beginning of the list
-            elif previous < 0:
-                if ratings[curr] > ratings[next]:
-                    min_num_of_candies += 1
-                    list_of_candy[curr] += 1
 
-            # case -> 3 when the current index is at the end of the list
-            elif next == len(ratings):
-                if ratings[curr] > ratings[previous]:
-                    min_num_of_candies += list_of_candy[previous]
-                    list_of_candy[curr] = list_of_candy[previous] + 1
+        # going from left to right and always comparing with the child who is behind the current child
+        for curr in range(1, list_size):
+            if ratings[curr] > ratings[curr-1]:
+                # no need to add one since the every child already has one
+                list_of_candy[curr] += list_of_candy[curr-1]
 
-        return min_num_of_candies
+        # going from right to left and always comparing with the child who is behind the current child
+        for curr in range(list_size-2, -1, -1):
+            # rating is higher but the number of candy is equal or lower
+            if ratings[curr] > ratings[curr+1]:
+                if list_of_candy[curr] <= list_of_candy[curr+1]:
+                    list_of_candy[curr] += list_of_candy[curr+1] - list_of_candy[curr] + 1
+
+        return sum(list_of_candy)
 
 
 if __name__ == '__main__':
     obj = Solution()
-    result = obj.candy([1,2,87,87,87,2,1])
+    result = obj.candy([1,6,10,8,7,3,2])
     print(result)
     # Input: ratings = [1, 0, 2]
     # Output: 5
