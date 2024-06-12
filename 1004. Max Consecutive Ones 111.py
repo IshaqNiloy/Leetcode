944bc2e8666a93e227760252c06c3c1f1df509af
IshaqNiloy <ishaqniloy1996@gmail.com>
2024-06-13 03:17:47 +0600

    solved at first attempt with dynamic sliding window technique that beats 49.06% of users with Python3

--- 1004. Max Consecutive Ones 111.py
+++ 1004. Max Consecutive Ones 111.py
@@ -9,22 +9,20 @@ logger.addHandler(handler)
 
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        zero_index_list, window_size, max_consecutive_ones, j = list(), 0, 0, 1
+        # sliding window technique
+        start, max_length, zero_counter = 0, 0, 0
         try:
-            for index, num in enumerate(nums):
-                if num == 0:
-                    zero_index_list.append(index)
-                if num == 0 and k != 0:
-                    k -= 1
-                elif num == 0 and k == 0:
-                    max_consecutive_ones = max(max_consecutive_ones, window_size)
-                    window_size = zero_index_list[j] - zero_index_list[j-1]
-                    j += 1
+            for end in range(len(nums)):
+                if nums[end] == 0:
+                    zero_counter += 1
 
-                window_size += 1
-            logger.info(f'window size: {window_size}')
+                while zero_counter > k:
+                    if nums[start] == 0:
+                        zero_counter -= 1
+                    start += 1
 
-            return max_consecutive_ones
+                max_length = max(max_length, end - start + 1)
+            return max_length
 
         except Exception as e:
             logger.info(f'exception: {e}')
