5041c3c00b5f78805c561eaade3a36c940cc0ba1
IshaqNiloy <ishaqniloy1996@gmail.com>
2024-06-14 23:38:31 +0600

    solved at fifth attempt that beats 99.11% of users with Python3

--- 26. Remove Duplicates from Sorted Array.py
+++ 26. Remove Duplicates from Sorted Array.py
@@ -3,14 +3,14 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        new_list = set(nums)
+        unique_nums_list = list(set(nums))
         nums.clear()
-        for item in new_list:
+        for item in sorted(unique_nums_list):
             nums.append(item)
-        print(nums)
+
         return len(nums)
 
 
 if __name__ == '__main__':
     obj = Solution()
-    print(obj.removeDuplicates([1,1,2]))
+    print(obj.removeDuplicates([-1,0,0,0,0,3,3]))
