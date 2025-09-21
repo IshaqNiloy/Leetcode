041ab4ffc2c11b6ab4ae7ffa1064386d2eb9e567
IshaqNiloy <ishaqniloy1996@gamil.com>
2025-09-22 00:50:07 +0600

    solved at first attempt that beats 80.89% of submissions

---
+++ TypeScript/36. Valid Sudoku.ts
@@ -0,0 +1,69 @@
+function isValidSudoku(board: string[][]): boolean {
+  try {
+    let numberFrequencyMapping: any = {};
+
+    // Check if rows are valid
+    for (const row of board) {
+      for (const num of row) {
+        if (num in numberFrequencyMapping) {
+          numberFrequencyMapping[num] += 1;
+        } else {
+          numberFrequencyMapping[num] = 1;
+        }
+      }
+      // Check if the the row has any duplicate number
+      if (numberFrequencyMapping["."] === 9) {
+        return false;
+      }
+      delete numberFrequencyMapping["."];
+      if (
+        Object.values(numberFrequencyMapping).some((count: any) => count > 1)
+      ) {
+        return false;
+      }
+      numberFrequencyMapping = {};
+    }
+
+    // Check if columns are valid
+    for (let column = 0; column < board.length; column++) {
+      for (let row = 0; row < board.length; row++) {
+        if (board[row][column] in numberFrequencyMapping) {
+          numberFrequencyMapping[board[row][column]] += 1;
+        } else {
+          numberFrequencyMapping[board[row][column]] = 1;
+        }
+      }
+      // Check if the the column has any duplicate number
+      if (numberFrequencyMapping["."] === 9) {
+        return false;
+      }
+      delete numberFrequencyMapping["."];
+      if (
+        Object.values(numberFrequencyMapping).some((count: any) => count > 1)
+      ) {
+        return false;
+      }
+      numberFrequencyMapping = {};
+    }
+    return true;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function main() {
+  const result = isValidSudoku([
+    ["1", "2", ".", ".", "3", ".", ".", ".", "."],
+    ["4", ".", ".", "5", ".", ".", ".", ".", "."],
+    [".", "9", "1", ".", ".", ".", ".", ".", "3"],
+    ["5", ".", ".", ".", "6", ".", ".", ".", "4"],
+    [".", ".", ".", "8", ".", "3", ".", ".", "5"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", ".", ".", ".", ".", ".", "2", ".", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "8"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+  ]);
+  console.log(result);
+}
+
+main();
