37dbcb27ba7c916d0945b2fdd272062c525a2fe8
IshaqNiloy <ishaqniloy1996@gamil.com>
2025-10-03 11:36:53 +0600

    solved at second attempt that beats 57.35% submissions

---
+++ Python/812. Largest Triangle Area.py
@@ -0,0 +1,20 @@
+from typing import List
+from itertools import combinations
+
+class Solution:
+    def largestTriangleArea(self, points: List[List[int]]) -> float:
+        def area(a, b, c) -> float:
+            (x1, y1), (x2, y2), (x3, y3) = a, b, c
+
+            return abs((x2 - x1)*(y3 - y1) - (y2 - y1)*(x3 - x1)) / 2.0
+
+        max_area = 0.0
+        for a, b, c in combinations(points, 3):
+            max_area = max(max_area, area(a, b, c))
+        return max_area
+
+        
+
+if __name__ == '__main__':
+    obj = Solution()
+    print(obj.largestTriangleArea([[4,6],[6,5],[3,1]]))
