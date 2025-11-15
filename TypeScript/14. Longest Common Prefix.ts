b966811f621a5ea0e3b9380af60ef132c01555d9
IshaqNiloy <ishaqniloy1996@gamil.com>
2025-11-15 19:33:18 +0600

    This solution beats 100% submissions

---
+++ TypeScript/14. Longest Common Prefix.ts
@@ -0,0 +1,22 @@
+function longestCommonPrefix(strs: string[]): string {
+  try {
+    let prefix: string = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+      while (strs[i].indexOf(prefix) !== 0) {
+        prefix = prefix.slice(0, prefix.length - 1);
+        if (prefix.length === 0) return "";
+      }
+    }
+
+    return prefix;
+  } catch (error) {
+    throw error;
+  }
+}
+
+function main() {
+  console.log(longestCommonPrefix(["dog", "racecar", "car"]));
+}
+
+main();
